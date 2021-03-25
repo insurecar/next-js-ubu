6f024e028e9db199c4272ea7ca82b220ba7a9430
insurecar <rostyslavshyian@gmail.com>
2021-03-25 17:17:05 +0200

    add server

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -1,11 +1,31 @@
 import { useRouter } from "next/router";
+import { MainLayout } from "../../components/MainLayout";
+import Link from "next/link";
 
-export default function Post() {
+export default function Post({ post }) {
   const router = useRouter();
-  console.log(router);
+  console.log("POST", post);
   return (
-    <>
-      <h1>Post {router.query.id}</h1>
-    </>
+    <MainLayout title={post.title}>
+      <h1>Post {post.title}</h1>
+      <hr />
+      <p>{post.body}</p>
+      <button>
+        <Link href="/posts">
+          <a>Back to posts</a>
+        </Link>
+      </button>
+    </MainLayout>
   );
 }
+
+Post.getInitialProps = async (ctx) => {
+  console.log(ctx);
+
+  const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`);
+  const post = await response.json();
+
+  return {
+    post,
+  };
+};
