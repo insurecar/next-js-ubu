69cad3030ded6455ce6df7b23caec2f2108f919a
insurecar <rostyslavshyian@gmail.com>
2021-03-26 09:56:25 +0200

    add indicate loading

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,23 @@
 import Router from "next/router";
 import { MainLayout } from "../../components/MainLayout";
+import { useState } from "react";
 
-export default function about() {
+export default function About({ title }) {
+  // const [] = useState(serverTitle);
   return (
     <MainLayout title={"About Page"}>
-      <h1>About page</h1>
+      <h1>{title}</h1>
       <button onClick={() => Router.push("/")}>Go back to home</button>
       <button onClick={() => Router.push("/posts")}>Go to posts</button>
     </MainLayout>
   );
 }
+
+About.getInitialProps = async () => {
+  const response = await fetch(`http://localhost:4200/about`);
+  const data = await response.json();
+
+  return {
+    title: data.title,
+  };
+};
